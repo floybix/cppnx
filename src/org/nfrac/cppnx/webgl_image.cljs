2b8d2ee3725f2bcc8960064dfaf4902646733124
Felix Andrews <felix@nfrac.org>
2016-11-09 15:45:14 +0800

    Add distance input, gaussian fn

--- src/org/nfrac/cppnx/webgl_image.cljs
+++ src/org/nfrac/cppnx/webgl_image.cljs
@@ -11,18 +11,18 @@
 ;; so all non-input nodes must have at least one input edge.
 ;; and cycles are not allowed.
 (def ex-cppn
-  {:inputs [:bias :x :y]
-   :nodes {3 :sin
+  {:inputs [:bias :x :y :d]
+   :nodes {3 :gauss
            4 :lin}
-   :deps {3 {:x 10}
+   :deps {3 {:y 1.0}
           4 {3 0.5
              :y -1.0}}
-   :out-nodes {:h :bias
+   :out-nodes {:h :d
                :s 4
-               :v 4}})
+               :v 3}})
 
 (declare v-position)
-(declare hsv2rgb)
+(declare hsv2rgb-glsl)
 
 (defn remap
   "Transforms a map `m` applying function `f` to each value."
@@ -30,6 +30,16 @@
   (into (or (empty m) {})
         (map (fn [[k v]] [k (f v)]))
         m))
+
+(defn gaussian-glsl
+  "2 * Math.exp(-Math.pow(inputSignal * 2.5, 2)) - 1"
+  [z]
+  (-> (g/* z 2.5)
+      (g/pow 2.0)
+      (g/* -1.0)
+      (g/exp)
+      (g/* 2.0)
+      (g/- 1.0)))
 
 (defn build-cppn-vals
   [cppn in-exprs]
@@ -46,7 +56,8 @@
                                    sumw (reduce g/+ (map g/abs (vals ideps)))
                                    expr (case node-type
                                           :sin (g/sin sum)
-                                          :lin (g/div sum sumw))]
+                                          :lin (g/div sum sumw)
+                                          :gauss (gaussian-glsl sum))]
                                 (assoc m nid expr)))
                            in-exprs
                            ;; topological sort
@@ -57,17 +68,20 @@
   [cppn]
   (let [x (g/swizzle v-position :x)
         y (g/swizzle v-position :y)
-        in-exprs {:bias 1.0, :x x, :y y}
+        d (g/- (g/* (g/sqrt (g/+ (g/* x x) (g/* y y)))
+                    (/ 2 (Math/sqrt 2.0)))
+               1.0)
+        in-exprs {:bias 1.0, :x x, :y y, :d d}
         out-exprs (build-cppn-vals cppn in-exprs)
         ;; for colours, convert [-1 1] to [0 1]
         out-exprs-01 (remap #(g/+ (g/* % 0.5) 0.5) out-exprs)]
-    {(g/gl-frag-color) (g/vec4 (hsv2rgb (:h out-exprs-01)
-                                        (:s out-exprs-01)
-                                        (:v out-exprs-01)) 1)}))
+    {(g/gl-frag-color) (g/vec4 (hsv2rgb-glsl (:h out-exprs-01)
+                                             (:s out-exprs-01)
+                                             (:v out-exprs-01)) 1)}))
 
 ;;; webgl
 
-(defn hsv2rgb
+(defn hsv2rgb-glsl
   "from http://stackoverflow.com/a/17897228/202244
 
 vec3 hsv2rgb(vec3 c)
@@ -101,7 +115,7 @@ vec3 hsv2rgb(vec3 c)
         y (g/swizzle v-position :y)
         x01 (g/+ (g/* x 0.5) 0.5)
         y01 (g/+ (g/* y 0.5) 0.5)]
-    {(g/gl-frag-color) (g/vec4 (hsv2rgb x01 1 y01) 1)}))
+    {(g/gl-frag-color) (g/vec4 (hsv2rgb-glsl x01 1 y01) 1)}))
 
 (def fragment-shader
   (xy-to-hsv-shader ex-cppn))
