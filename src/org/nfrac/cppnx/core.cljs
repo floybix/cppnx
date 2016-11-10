ba396ee75a09c9c3865ae3e647ea8b8b43e7e6b6
Felix Andrews <felix@nfrac.org>
2016-11-10 12:43:47 +0800

    A basic spec for cppns

--- src/org/nfrac/cppnx/core.cljs
+++ src/org/nfrac/cppnx/core.cljs
@@ -1,11 +1,44 @@
 (ns org.nfrac.cppnx.core
   (:require [gamma.api :as g]
-            [org.nfrac.cppnx.util.algo-graph :as graph]))
+            [org.nfrac.cppnx.util.algo-graph :as graph]
+            [clojure.spec :as s]
+            [clojure.spec.impl.gen :as gen]))
 
-;;; cppns
+;; network topology is defined by dependencies between nodes.
+;; so all non-input nodes must have at least one input edge.
+;; and cycles are not allowed.
+(def example-cppn
+  {:inputs [:bias :x :y :d]
+   :nodes {3 :gaussian
+           4 :linear}
+   :edges {3 {:y 1.0}
+           4 {3 0.5
+              :y -1.0}}
+   :out-nodes {:h :d
+               :s 4
+               :v 3}
+   :topology-hash 0})
 
 (def all-node-types
-  #{:linear :sine :gaussian})
+  #{:linear :sine :gaussian :sigmoid :sawtooth})
+
+(s/def ::node-id (-> any? (s/with-gen #(s/gen ident?))))
+(s/def ::inputs (s/coll-of ::node-id, :min-count 1, :distinct true))
+(s/def ::nodes (s/map-of ::node-id all-node-types))
+(s/def ::weight (s/double-in :min -100 :max 100 :NaN? false))
+(s/def ::node-edges (s/map-of ::node-id ::weight))
+(s/def ::edges (s/map-of ::node-id ::node-edges))
+(s/def ::out-nodes (s/map-of keyword? ::node-id))
+(s/def ::topology-hash int?)
+
+(s/def ::cppn
+  (s/keys :req-un [::inputs
+                   ::nodes
+                   ::edges
+                   ::out-nodes
+                   ::topology-hash]))
+
+;;; cppns
 
 (defmulti node-glsl
   (fn [type sum weight]
@@ -28,20 +61,25 @@
       (g/* 2.0)
       (g/- 1.0)))
 
-;; network topology is defined by dependencies between nodes.
-;; so all non-input nodes must have at least one input edge.
-;; and cycles are not allowed.
-(def example-cppn
-  {:inputs [:bias :x :y :d]
-   :nodes {3 :gaussian
-           4 :linear}
-   :deps {3 {:y 1.0}
-          4 {3 0.5
-             :y -1.0}}
-   :out-nodes {:h :d
-               :s 4
-               :v 3}
-   :topology-hash 0})
+(defmethod node-glsl :sigmoid
+  [_ sum _]
+  (->
+   (g/div
+    1.0
+    (-> (g/* sum -4.9)
+        (g/exp)
+        (g/+ 1.0)
+        (g/div 1.0)))
+   (g/* 2.0)
+   (g/- 1.0)))
+
+(defmethod node-glsl :sawtooth
+  [_ sum _]
+  (-> (g/fract sum)
+      (g/* 2.0)
+      (g/- 1.0)))
+
+;;; cppn wrangling
 
 (defn mutate-add-node
   [cppn])
@@ -84,7 +122,7 @@
 (defn cppn-strata
   [cppn]
   (let [gr (graph/directed-graph (concat (:inputs cppn) (keys (:nodes cppn)))
-                                 (remap keys (:deps cppn)))]
+                                 (remap keys (:edges cppn)))]
     (graph/dependency-list gr)))
 
 (defn build-cppn-vals
@@ -93,7 +131,7 @@
         sorted-nids (apply concat (rest strata))
         node-exprs (reduce (fn [m nid]
                              (let [node-type (get-in cppn [:nodes nid])
-                                   ideps (get-in cppn [:deps nid])
+                                   ideps (get-in cppn [:edges nid])
                                    sum (->> ideps
                                             (map (fn [[from-id w]]
                                                    (g/* w (get m from-id))))
