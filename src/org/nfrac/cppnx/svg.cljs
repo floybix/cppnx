76377ff0ea719dd96b948497ffd64f129ef19d30
Felix Andrews <felix@nfrac.org>
2016-12-06 14:02:49 +0800

    Support touch interface for svg drags

--- src/org/nfrac/cppnx/svg.cljs
+++ src/org/nfrac/cppnx/svg.cljs
@@ -9,12 +9,39 @@
 
 (def dragging (atom nil))
 
+;; prevent scrolling when dragging
+(defonce window-touchmove
+  (aset js/window "ontouchmove"
+        (fn [e]
+          (when @dragging
+            (.preventDefault e)))))
+
 (defn offset-from-svg
   [e]
   (let [el (dom/getElementByClass "cppn-graph")
         r (.getBoundingClientRect el)]
     [(- (.-clientX e) (.-left r))
      (- (.-clientY e) (.-top r))]))
+
+(defn abs [x] (if (neg? x) (- x) x))
+
+(defn find-node-at-xy
+  [by-node x y rx ry]
+  (some (fn [[node info]]
+          (let [nx (:x info)
+                ny (:y info)]
+            (when (and (< (abs (- nx x)) rx)
+                       (< (abs (- ny y)) ry))
+              node)))
+        by-node))
+
+(defn valid-drop?
+  [cppn from to]
+  (let [final? (cppnx/finals cppn)
+        input? (:inputs cppn)]
+    (and from to (not= from to)
+         (not (and (final? from) (final? to)))
+         (not (and (input? from) (input? to))))))
 
 (defn cppn-svg
   [cppn selection event-c]
@@ -39,7 +66,17 @@
                       (let [[x y] (offset-from-svg e)]
                         (js/requestAnimationFrame
                          (fn [_]
-                           (swap! dragging assoc :at [x y]))))))
+                           (let [targ (find-node-at-xy by-node x y radius-x radius-y)
+                                 ok? (when targ
+                                       (valid-drop? cppn (:node @dragging) targ))]
+                             (swap! dragging assoc :at [x y]
+                                    :target (when ok? targ))))))))
+        drop (fn [e]
+               (when (:target @dragging)
+                 (put! event-c {:event :link
+                                :from (:node @dragging)
+                                :to (:target @dragging)}))
+               (reset! dragging nil))
         bg-click (fn [e]
                    (.preventDefault e)
                    (put! event-c {:event :select
@@ -50,9 +87,12 @@
               :border "solid 1px black"
               :font-size "12px"}
       :onMouseMove drag-move
-      :onTouchMove drag-move
+      :onTouchMove (fn [e]
+                     (drag-move (aget (.-changedTouches e) 0)))
       :onMouseLeave (fn [e] (reset! dragging nil))
-      :onMouseUp (fn [e] (reset! dragging nil))
+      :onMouseUp drop
+      :onTouchEnd (fn [e]
+                    (drop (aget (.-changedTouches e) 0)))
       :onClick bg-click}
      ;; edges
      (into
@@ -72,40 +112,16 @@
          [:polyline
           {:points (str/join " " [(:x info) (:y info)
                                   x y])
-           :stroke (if (:on-target? @dragging) "#0f0" "#888")
+           :stroke (if (:target @dragging) "#0f0" "#888")
            :stroke-width 2}]))
      ;; nodes
      (into
       [:g]
       (for [[node info] by-node
-            :let [valid-drop? (fn []
-                                (let [from (:node @dragging)
-                                      final? (cppnx/finals cppn)
-                                      input? (:inputs cppn)]
-                                  (and from node (not= from node)
-                                       (not (and (final? from) (final? node)))
-                                       (not (and (input? from) (input? node))))))
-                  drag-start (fn [e]
+            :let [drag-start (fn [e]
                                (let [[x y] (offset-from-svg e)]
                                  (reset! dragging {:node node
                                                    :at [x y]})))
-                  drag-enter (fn [e]
-                               (when @dragging
-                                 (.preventDefault e)
-                                 (when (valid-drop?)
-                                   (swap! dragging assoc :on-target? true))))
-                  drag-touch (fn [e]
-                               (if @dragging (drag-enter e) (drag-start e)))
-                  drag-leave (fn [e]
-                               (when @dragging
-                                 (.preventDefault e)
-                                 (swap! dragging assoc :on-target? false)))
-                  drop (fn [e]
-                         (.preventDefault e)
-                         (when (valid-drop?)
-                           (put! event-c {:event :link
-                                          :from (:node @dragging)
-                                          :to node})))
                   click (fn [e]
                           (.preventDefault e)
                           (.stopPropagation e)
@@ -123,17 +139,13 @@
            :stroke-width (if (zerod? node) "4" "1")
            ;; note - SVG doesn't support actual drag&drop events
            :onMouseDown drag-start
-           :onMouseEnter drag-enter
-           :onMouseLeave drag-leave
-           :onMouseUp drop
-           :onTouchStart drag-touch
-           :onTouchEnd drop ;; could be leave or drop
+           :onTouchStart (fn [e]
+                           (drag-start (aget (.-changedTouches e) 0)))
            :onClick click
            :style {:cursor "alias"
                    :user-select "none"}}]
          [:text
-          {:style {:pointer-events "none"
-                   :user-select "none"}
+          {:style {:pointer-events "none"}
            :text-anchor "middle"
            :x (:x info)
            :y (+ (:y info) 4)}
